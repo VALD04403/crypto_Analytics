743ce909f0ba2fe23acc479cf53a2b4dd6c94957
Valentin Berruer <valb37@icloud.com>
2020-04-30 17:40:31 +0200

    calcul solde portefeuille et pourcentage plus value/moins value

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Dashboard from './container/Dashboard';
 import contextPurchases from './context/contextPurchases';
 
 function App() {
-  // const contextValue = {
-  //   shouldRefecthPurchases
-  // };
+  const [location, setLocation] = useState('/dashboard');
+
+  const contextValue = {
+    location,
+  };
 
   return (
     <div className="App">
-      <contextPurchases.Provider>
+      <contextPurchases.Provider value={contextValue}>
         <Route path="/" component={() => <Dashboard />} />
       </contextPurchases.Provider>
     </div>
--- webapp/src/components/Card.js
+++ webapp/src/components/Card.js
@@ -1,15 +1,13 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { Card, Grid } from 'semantic-ui-react';
+import { Card, Grid, Header } from 'semantic-ui-react';
 
 function CardWallet() {
+  let totalSpend = 0;
   const [walletValue, setWalletValue] = useState();
+  const [percent, setPercent] = useState();
   const [total, setTotal] = useState();
   const [fees, setFees] = useState();
   const [totalWithFees, setTotalWithFees] = useState();
-
-  // const getTotalWallet = async () => {
-  //   const response = await fetch('/api/totalWallet');
-  // };
 
   const getGeneralData = async () => {
     const data = await fetch('/api/info');
@@ -17,14 +15,59 @@ function CardWallet() {
       res.data.map((info) => {
         setTotal(info.total_invest);
         setFees(info.total_fees);
-        setTotalWithFees(info.total_with_fees);
+        setTotalWithFees(Number(info.total_invest) + Number(info.total_fees));
+        totalSpend = Number(info.total_invest) + Number(info.total_fees);
+      });
+    });
+  };
+
+  const getTotalValueWallet = async () => {
+    const orderItems = [];
+    const listItems = [];
+    const totalAmountByCoin = [];
+    const total = [];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const purschases = await fetch('/api/purchases');
+    purschases.json().then((res) => {
+      res.purchases.map((item) => {
+        if (listItems.indexOf(item.coin_name) === -1) {
+          const coin = {
+            name: item.coin_name,
+            purchases: [],
+          };
+          orderItems.push(coin);
+          listItems.push(item.coin_name);
+        }
+        orderItems.map((purchase) => {
+          if (purchase.name === item.coin_name) {
+            purchase.purchases.push(item);
+          }
+        });
+      });
+      orderItems.map((item) => {
+        const amount = item.purchases.reduce(function (res, item) {
+          return res + parseFloat(item.amount_coin);
+        }, 0);
+        totalAmountByCoin.push({ name: item.name, amount: amount });
+      });
+      totalAmountByCoin.map(async (item) => {
+        const value = await fetch(`/api/value/${item.name}`, {
+          headers: { 'Content-Type': 'application/json' },
+          method: 'GET',
+        });
+        await value.json().then((res) => {
+          total.push(res.response * item.amount);
+        });
+        setWalletValue(total.reduce(reducer));
+        const calculPercent = (totalSpend * 100) / total.reduce(reducer);
+        setPercent((100 - Number(calculPercent)).toFixed(2));
       });
     });
   };
 
   useEffect(() => {
     getGeneralData();
-    // getTotalWallet();
+    getTotalValueWallet();
   }, []);
 
   return (
@@ -34,16 +77,26 @@ function CardWallet() {
           <Card.Header as="h1">Solde du portefeuille</Card.Header>
         </Card.Content>
         <Card.Content>
-          <Grid columns={3} divided>
+          <Grid columns={5} divided>
             <Grid.Row>
               <Grid.Column>
-                Total placé: <h2>{total} € </h2>
+                Fond investi: <h2>{total} € </h2>
               </Grid.Column>
               <Grid.Column>
-                Frais: <h2>{fees} €</h2>
+                Frais: <h3>{fees} €</h3>
               </Grid.Column>
               <Grid.Column>
-                Total avec frais: <h2>{totalWithFees} €</h2>
+                Total avec frais: <h3>{totalWithFees} €</h3>
+              </Grid.Column>
+              <Grid.Column>
+                Solde portefeuille:{' '}
+                <h2>{walletValue && walletValue.toFixed(2)} €</h2>
+              </Grid.Column>
+              <Grid.Column>
+                {percent > 0 ? 'Plus-value' : ' Moins-value'} portefeuille:{' '}
+                <Header as="h3" color={percent > 0 ? 'green' : 'red'}>
+                  {percent} %
+                </Header>
               </Grid.Column>
             </Grid.Row>
           </Grid>
