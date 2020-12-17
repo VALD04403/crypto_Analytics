14467c4275347ba0aad1aea4aae07159baf70f59
Valentin Berruer <v.berruer@monkey-tie.com>
2020-12-17 23:03:49 +0100

    prices component css mobile screen

--- webapp/src/components/Prices.js
+++ webapp/src/components/Prices.js
@@ -7,6 +7,8 @@ import {
   Dimmer,
   Loader,
   Button,
+  Divider,
+  Grid,
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
@@ -26,6 +28,8 @@ import axios from 'axios';
 
 function Prices() {
   const [list, setList] = useState();
+  const [mobileScreen, setMobileScreen] = useState(false);
+
   const listCoin = [
     'BTC',
     'ETH',
@@ -45,7 +49,25 @@ function Prices() {
     setList(data.data.response.Data);
   };
 
+  const numberWithSpaces = (value) => {
+    if (value) {
+      var parts = value.toString().split('.');
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+      parts[1] = parts[1].slice(0, 2);
+      return parts.join('.');
+    }
+  };
+
+  const windowResize = () => {
+    window.innerWidth < 768 ? setMobileScreen(true) : setMobileScreen(false);
+  };
+
   useEffect(() => {
+    windowResize();
+    function handleResize() {
+      windowResize();
+    }
+    window.addEventListener('resize', handleResize);
     getValueList();
     window.scrollTo(0, 0);
   }, []);
@@ -54,7 +76,7 @@ function Prices() {
     <Fragment>
       <Card style={{ width: '100%' }}>
         <Card.Content>
-          {list ? (
+          {list && !mobileScreen ? (
             <Table basic='very'>
               <Table.Header>
                 <Table.Row>
@@ -118,7 +140,10 @@ function Prices() {
                       </Table.Cell>
                       <Table.Cell>
                         <Header style={{ fontWeight: '200' }} as='h3'>
-                          {Math.round(item.RAW.EUR.PRICE * 100) / 100} €
+                          {numberWithSpaces(
+                            Math.round(item.RAW.EUR.PRICE * 100) / 100
+                          )}{' '}
+                          €
                         </Header>
                       </Table.Cell>
                       <Table.Cell>
@@ -152,6 +177,86 @@ function Prices() {
                   ))}
               </Table.Body>
             </Table>
+          ) : list && mobileScreen ? (
+            list.map((item) => (
+              <Card.Content key={item.CoinInfo.Id}>
+                {listCoin.includes(item.CoinInfo.Name) && (
+                  <Image
+                    style={{
+                      width: '30px',
+                      height: '30px',
+                    }}
+                    src={
+                      item.CoinInfo.Name === 'BTC'
+                        ? btc
+                        : item.CoinInfo.Name === 'ETH'
+                        ? eth
+                        : item.CoinInfo.Name === 'ETC'
+                        ? etc
+                        : item.CoinInfo.Name === 'XTZ'
+                        ? xtz
+                        : item.CoinInfo.Name === 'LTC'
+                        ? ltc
+                        : item.CoinInfo.Name === 'XRP'
+                        ? xrp
+                        : item.CoinInfo.Name === 'EOS'
+                        ? eos
+                        : item.CoinInfo.Name === 'KNC'
+                        ? knc
+                        : item.CoinInfo.Name === 'BCH'
+                        ? bch
+                        : item.CoinInfo.Name === 'XLM'
+                        ? xlm
+                        : item.CoinInfo.Name === 'LINK'
+                        ? link
+                        : item.CoinInfo.Name === 'BSV'
+                        ? bsv
+                        : ''
+                    }
+                  />
+                )}
+                <b style={{ marginLeft: '10px', fontSize: '16px' }}>
+                  {' '}
+                  {item.CoinInfo.FullName}
+                </b>
+                <span style={{ marginLeft: '10px' }}>{item.CoinInfo.Name}</span>
+
+                <p
+                  style={{
+                    fontWeight: '200',
+                    fontSize: '16px',
+                    marginTop: '1em',
+                    marginBottom: '1em',
+                  }}
+                >
+                  {numberWithSpaces(Math.round(item.RAW.EUR.PRICE * 100) / 100)}{' '}
+                  €
+                  <b
+                    style={{
+                      display: 'initial',
+                      marginLeft: '15px',
+                      color: item.RAW.EUR.CHANGEPCT24HOUR > 0 ? 'green' : 'red',
+                    }}
+                  >
+                    {item.RAW.EUR.CHANGEPCT24HOUR > 0 && '+'}{' '}
+                    {Math.round(item.RAW.EUR.CHANGEPCT24HOUR * 100) / 100} %
+                  </b>
+                </p>
+                {listCoin.includes(item.CoinInfo.Name) && (
+                  <Button
+                    style={{
+                      backgroundColor: '#1652F0',
+                      color: '#fff',
+                    }}
+                    as={Link}
+                    to='/portefeuille'
+                  >
+                    Ajouter
+                  </Button>
+                )}
+                {list.indexOf(item) !== list.length - 1 && <Divider></Divider>}
+              </Card.Content>
+            ))
           ) : (
             <Dimmer active style={{ height: '200px' }}>
               <Loader />
